86698bf3a42fad18e14a47c09b28d791e2eb235a
Oleg Namozov <oleg8494@yandex.ru>
2025-05-30 09:37:30 +0300

    added homework

---
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -0,0 +1,44 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackDriver implements WebDriverProvider {
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
+
+        // Set your access credentials
+        caps.setCapability("browserstack.user", "olegn_4osMUt");
+        caps.setCapability("browserstack.key", "5cWCPNSQgz4oq7qSkwCi");
+
+        // Set URL of the application under test
+        caps.setCapability("app", "bs://sample.app");
+
+        // Specify device and os_version for testing
+        caps.setCapability("device", "Google Pixel 7 Pro");
+        caps.setCapability("os_version", "13.0");
+
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+        try {
+            return new RemoteWebDriver(
+                    new URL("https://hub.browserstack.com/wd/hub"), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/tests/SearchJUnitTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
+public class SearchJUnitTests extends TestBase {
+
+    @Test
+    void successfulSearchTest() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("JUnit5");
+        });
+
+        step("Verify content found", () ->
+                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+
+    }
+}
+
